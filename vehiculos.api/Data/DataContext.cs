bc2ce8b7c183abdbde4fece7f7bd39d05964fea4
Andres Viera <andyviera212@hotmail.com>
2021-08-29 10:46:42 -0500

    Agregamos la tabla tipo de vehiculos

---
+++ vehiculos.api/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vehiculos.api.Data.Entities;
+
+namespace vehiculos.api.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<TipoVehiculo> TipoVehiculo { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TipoVehiculo>().HasIndex(x => x.Nombre).IsUnique(); 
+        }
+    }
+}
---
+++ vehiculos.api/Migrations/20210829153447_addTableTipoVehiculo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vehiculos.api.Migrations
+{
+    public partial class addTableTipoVehiculo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoVehiculo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoVehiculo", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TipoVehiculo_Nombre",
+                table: "TipoVehiculo",
+                column: "Nombre",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TipoVehiculo");
+        }
+    }
+}
